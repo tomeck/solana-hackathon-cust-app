8ef7620249df522da51fc3d5a3202820d7a826aa
tomeck <dr.teck@gmail.com>
2021-02-23 18:14:04 -0500

    v1.0   Added short history.  Fully working e2e

--- Circle Consumer Wallet/ViewController.h
+++ Circle Consumer Wallet/ViewController.h
@@ -11,6 +11,14 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *lblBalance;
 @property (weak, nonatomic) IBOutlet UILabel *lblCurrency;
+@property (weak, nonatomic) IBOutlet UILabel *lblAmt1;
+@property (weak, nonatomic) IBOutlet UILabel *lblDate1;
+@property (weak, nonatomic) IBOutlet UILabel *lblStatus1;
+@property (weak, nonatomic) IBOutlet UILabel *lblID1;
+@property (weak, nonatomic) IBOutlet UILabel *lblAmt2;
+@property (weak, nonatomic) IBOutlet UILabel *lblDate2;
+@property (weak, nonatomic) IBOutlet UILabel *lblStatus2;
+@property (weak, nonatomic) IBOutlet UILabel *lblID2;
 
 -(IBAction)unwindToNewTransactionViewController:(UIStoryboardSegue *)segue;
 
--- Circle Consumer Wallet/ViewController.m
+++ Circle Consumer Wallet/ViewController.m
@@ -20,6 +20,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [self getBalance];
+    [self getRecentTransactions];
 }
 
 
@@ -27,6 +28,7 @@
 // landing here
 -(IBAction)unwindToNewTransactionViewController:(UIStoryboardSegue *)segue {
     [self getBalance];
+    [self getRecentTransactions];
 }
 
 
@@ -81,43 +83,33 @@
                 }
             }
         });
-
-       
     }];
     
     // Execute the task
     [task resume];
 }
 
-// JTE TESTING ONLY
-/*
-- (IBAction)testPayClicked:(id)sender {
-    NSString *scannedText = @"FUoAafzWRYp8dsshzKqadN7QXGZQAJ6M5dc95jN1d9GJ|Sal's Pizza|23.45";
-    
-    NSDictionary *reqDict = [self getTransferDictForText:scannedText];
+- (void)getRecentTransactions {
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     
     // Create NSURLSession object.
     NSURLSession *session = [NSURLSession sharedSession];
     
     // Create a NSURL object.
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/transfers", appDelegate.CIRCLE_API_BASE_URL]];
-
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/transfers?sourceWallet=%@", appDelegate.CIRCLE_API_BASE_URL, appDelegate.CONSUMER_WALLET_ID]];
+    //NSLog(@"%@", [NSString stringWithFormat:@"About to invoke %@", [url absoluteString]]);
     NSLog(@"About to invoke %@", [url absoluteString]);
-    NSLog(@"Request body is %@", reqDict);
-    
-    // Convert NSDictionary that contains kv's into JSON and then encoded within NSData
-    NSData *jsonBodyData = [NSJSONSerialization dataWithJSONObject:reqDict options:kNilOptions error:nil];
- 
+
+
     // Create post request object with the url.
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     // Set request method to POST
-    request.HTTPMethod = @"POST";
+    request.HTTPMethod = @"GET";
     // Set request body
-    request.HTTPBody = jsonBodyData;
-    [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
-
+    //request.HTTPBody = jsonBodyData;
+    
     // Set auth header
-    [request setValue:[NSString stringWithFormat:@"Bearer %@", API_KEY] forHTTPHeaderField:@"Authorization"];
+    [request setValue:[NSString stringWithFormat:@"Bearer %@", appDelegate.CIRCLE_API_KEY] forHTTPHeaderField:@"Authorization"];
     
     // Create the NSURLSessionDataTask post task object
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
@@ -126,14 +118,14 @@
         dispatch_async(dispatch_get_main_queue(), ^{
             
             if( error ) {
-                NSLog( @"Failed to transfer funds to merchant, error = %@", error );
+                NSLog( @"Failed to obtain balance for Wallet ID for %@, error = %@", appDelegate.CONSUMER_WALLET_ID, error );
             }
             else {
                 NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *) response;
                 long httpStatus = httpResp.statusCode;
                 
-                if( httpStatus != 201) {
-                    NSLog( @"Failed to transfer funds to merchant, httpStatus = %ld", httpStatus );
+                if( httpStatus != 200) {
+                    NSLog( @"Failed to obtain balance for Wallet ID for %@, httpStatus = %ld", appDelegate.CONSUMER_WALLET_ID, httpStatus );
                 }
                 else {
                     
@@ -141,111 +133,27 @@
                     NSDictionary *dictResponse = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
                     // And dump the dictionary
                     NSLog(@"%@", dictResponse);
-                    //self.lblBalance.text = dictResponse[@"data"][@"balances"][0][@"amount"];
-                    //self.lblCurrency.text = dictResponse[@"data"][@"balances"][0][@"currency"];
                     
-                    // Refresh screen to see updated balance
-                    [self getBalance];
+                    //
+                    // Load up the recent history
+                    // Most recent txn
+                    self.lblAmt1.text = [NSString stringWithFormat:@"%@  %@", dictResponse[@"data"][0][@"amount"][@"amount"], dictResponse[@"data"][0][@"amount"][@"currency"]];
+                    self.lblDate1.text = dictResponse[@"data"][0][@"createDate"];
+                    self.lblStatus1.text = dictResponse[@"data"][0][@"status"];
+                    self.lblID1.text = dictResponse[@"data"][0][@"id"];
+
+                    // Prior txn
+                    self.lblAmt2.text = [NSString stringWithFormat:@"%@  %@", dictResponse[@"data"][1][@"amount"][@"amount"], dictResponse[@"data"][1][@"amount"][@"currency"]];
+                    self.lblDate2.text = dictResponse[@"data"][1][@"createDate"];
+                    self.lblStatus2.text = dictResponse[@"data"][1][@"status"];
+                    self.lblID2.text = dictResponse[@"data"][1][@"id"];
                 }
             }
         });
-
-       
     }];
     
     // Execute the task
     [task resume];
 }
 
-- (NSDictionary *)getTransferDictForText:(NSString *)text {
-
-    // Split the scanned text by the pipe delimiter
-    NSArray *splits = [text componentsSeparatedByString:@"|"];
-    NSString *chainAddress = splits[0];
-    NSString *merchantName = splits[1];
-    NSString *amount = splits[2];
-
-    // Load up a Dictionary with the request body
-    //{"idempotencyKey": "dbbc9d81-72f1-4bf8-97c8-99f1f28995f2",
-    //  "source": {"type": "wallet", "id": "1000072203"},
-    //  "destination": {"type": "blockchain", "address": "FUoAafzWRYp8dsshzKqadN7QXGZQAJ6M5dc95jN1d9GJ", "chain": "SOL"},
-    //  "amount": {"amount": "0.20", "currency": "USD"}}    NSDictionary *sourceDict = @{ @"type" : @"wallet", @"id" : CONSUMER_WALLET_ID };
-    NSDictionary *sourceDict = @{ @"type" : @"wallet", @"id" : CONSUMER_WALLET_ID };
-    NSDictionary *destDict = @{ @"type" : @"blockchain", @"address" : chainAddress, @"chain" : @"SOL"};
-    NSDictionary *amountDict = @{ @"amount": amount, @"currency": @"USD" };
-    NSDictionary *reqDict = @{ @"idempotencyKey" :[[NSUUID UUID] UUIDString], @"source" : sourceDict, @"destination" : destDict, @"amount" : amountDict };
-    
-    return reqDict;
-}
-*/
-
-/*
-- (IBAction)btnPayNow:(id)sender {
-    
-
-    // Load up a Dictionary with the request body
-    //{"idempotencyKey": "dbbc9d81-72f1-4bf8-97c8-99f1f28995f2",
-    //  "source": {"type": "wallet", "id": "1000072203"},
-    //  "destination": {"type": "blockchain", "address": "FUoAafzWRYp8dsshzKqadN7QXGZQAJ6M5dc95jN1d9GJ", "chain": "SOL"},
-    //  "amount": {"amount": "0.20", "currency": "USD"}}
-    NSDictionary *sourceDict = @{ @"type" : @"wallet", @"id" : CONSUMER_WALLET_ID };
-    NSDictionary *destDict = @{ @"type" : @"blockchain", @"address" : MERCHANT_SOLANA_ADDRESS, @"chain" : @"SOL"};
-    NSDictionary *amountDict = @{ @"amount": @"0.10", @"currency": @"USD" };
-    NSDictionary *reqDict = @{ @"idempotencyKey" :[[NSUUID UUID] UUIDString], @"source" : sourceDict, @"destination" : destDict, @"amount" : amountDict };
-    
-    NSLog(@"About to invoke %@", [url absoluteString]);
-    NSLog(@"Request body is %@", reqDict);
-    
-    // Convert NSDictionary that contains kv's into JSON and then encoded within NSData
-    NSData *jsonBodyData = [NSJSONSerialization dataWithJSONObject:reqDict options:kNilOptions error:nil];
- 
-    // Create post request object with the url.
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    // Set request method to POST
-    request.HTTPMethod = @"POST";
-    // Set request body
-    request.HTTPBody = jsonBodyData;
-    [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
-
-    // Set auth header
-    [request setValue:[NSString stringWithFormat:@"Bearer %@", API_KEY] forHTTPHeaderField:@"Authorization"];
-    
-    // Create the NSURLSessionDataTask post task object
-    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        // We need to dispatch this to the main/UI thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            if( error ) {
-                NSLog( @"Failed to transfer funds to merchant, error = %@", error );
-            }
-            else {
-                NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *) response;
-                long httpStatus = httpResp.statusCode;
-                
-                if( httpStatus != 201) {
-                    NSLog( @"Failed to transfer funds to merchant, httpStatus = %ld", httpStatus );
-                }
-                else {
-                    
-                    // Deserialize the Http response into NSDictionary
-                    NSDictionary *dictResponse = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-                    // And dump the dictionary
-                    NSLog(@"%@", dictResponse);
-                    //self.lblBalance.text = dictResponse[@"data"][@"balances"][0][@"amount"];
-                    //self.lblCurrency.text = dictResponse[@"data"][@"balances"][0][@"currency"];
-                    
-                    // Refresh screen to see updated balance
-                    [self getBalance];
-                }
-            }
-        });
-
-       
-    }];
-    
-    // Execute the task
-    [task resume];
-}
-*/
 @end
